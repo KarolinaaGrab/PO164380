2b7de489e933ced6838388d6b00b331523e542b4
KarolinaaGrab <92163557+KarolinaaGrab@users.noreply.github.com>
2022-10-10 16:02:59 +0200

    lab1.1

---
+++ lab1/zad1.py
@@ -0,0 +1,36 @@
+lista = []
+#a)
+
+# for element in range(10):
+#     lista.append(element+1)
+# print(lista)
+
+#b)
+# for element in range(11):
+#     lista.append(2*element)
+# print(lista)
+
+# c)
+# for element in range(10):
+#     lista.append((element+1)**2)
+# print(lista)
+
+# #d)
+# for element in range(10):
+#     lista.append(element*0)
+# print(lista)
+
+# e)
+# for element in range(10):
+#     if element % 2 == 0:
+#         lista.append(0)
+#     else:
+#         lista.append(1)
+# print(lista)
+
+# f)
+# for element in range(10):
+#     lista.append(element % 5)
+# print(lista)
+
+
---
+++ lab1/zad2.py
@@ -0,0 +1,9 @@
+ile = 0
+lista = [-3, -2, -1, 0, 1, 2, 3]
+
+
+def ile_ujemnych(lista):
+    for i in range(len(lista)):
+        if(element)
+        return ile + 1
+print(ile)
